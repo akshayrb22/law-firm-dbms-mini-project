241dc091d3f31837ae48aa0b9269da69e09f17ab
Akshay Ram Bhat <akshay.bhat981222@gmail.com>
2018-11-10 00:35:33 +0530

    Added create commands to SQLiteQueries. Need to fix DataAccess.cs.

---
+++ LawFirmDBMS/LawFirmDBMS/SQLiteQueries/AlterLawyer.sql
@@ -0,0 +1,3 @@
+﻿-- Script Date: 10-Nov-18 12:30 AM  - ErikEJ.SqlCeScripting version 3.5.2.75
+-- Adding as column with NOT NULL is not allowed, set a default value or allow NULL
+ALTER TABLE [LAWYER] ADD [PASSWORD] TEXT DEFAULT '' NOT NULL ;
